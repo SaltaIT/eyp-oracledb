fdd1e013d9fcf9f7159d068e84ccca3bb7b61771
Jordi Prats <jprats@systemadmin.es>
2016-06-22 11:34:13 +0200

    canvi fail per err

--- manifests/preinstallchecks.pp
+++ manifests/preinstallchecks.pp
@@ -11,16 +11,16 @@ class oracledb::preinstallchecks() inherits oracledb::params {
         ($::eyp_grub2_kernel_cmdline  =~ /transparent_hugepage=never/)
       )
   {
-    fail('transparent_hugepage=never not present, reboot required nigga')
+    err('transparent_hugepage=never not present, reboot required nigga')
   }
 
   if($current_mode!='disabled')
   {
-    fail('SELinux not disabled, reboot required nigga')
+    err('SELinux not disabled, reboot required nigga')
   }
 
   if($::fqdn !~ /(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)/)
   {
-    fail('As the point five of the environment section says (Requirements for Installing Oracle Database 12.1 on RHEL6 or OL6 64-bit (x86-64) (Doc ID 1529864.1)), host\'s name must be a FQDN')
+    err('As the point five of the environment section says (Requirements for Installing Oracle Database 12.1 on RHEL6 or OL6 64-bit (x86-64) (Doc ID 1529864.1)), host\'s name must be a FQDN')
   }
 }
