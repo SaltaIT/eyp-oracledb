35873e8044133dfa12fec340d80ee0dc5ea89eee
Jordi Prats <jordi.prats@gmail.com>
2020-05-28 14:06:31 +0200

    prova

--- manifests/init.pp
+++ manifests/init.pp
@@ -101,7 +101,7 @@ class oracledb(
 
   if($sysctl_vm_nr_hugepages==undef)
   {
-    $sysctl_vm_nr_hugepages_value_pre=$sga_mb*1024/2
+    $sysctl_vm_nr_hugepages_value_pre=$sga_mb/2
     $sysctl_vm_nr_hugepages_value=ceiling(sprintf('%f', $sysctl_vm_nr_hugepages_value_pre+2))
   }
   else
